e0d8837e5574547de699769e349eaefdeb3ef3e9
razvan-turiatca <codingwithrazvan@gmail.com>
2022-11-25 18:05:31 +0200

    another solution to the collect even values challenge

--- Recursion/Collect-Even-Values/index.js
+++ Recursion/Collect-Even-Values/index.js
@@ -14,3 +14,19 @@ const collectEvenValues = (arr) => {
 
   return newArr
 }
+
+// a different approach
+
+const collectEvenValues2 = (arr) => {
+  let newArr = []
+
+  if (arr.length === 0) return
+
+  if (arr[0] % 2 == 0) {
+    newArr.push(arr[0])
+  }
+
+  newArr = newArr.concat(collectEvenValues2(arr.slice(1)))
+
+  return newArr
+}
