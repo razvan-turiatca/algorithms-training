47a252188685cfb2d155fd9ab52d695182bcf35e
razvan-turiatca <codingwithrazvan@gmail.com>
2022-12-08 23:03:06 +0200

    solved flatten problem recursively

---
+++ Recursion/Flatten/index.js
@@ -0,0 +1,13 @@
+// my solution
+
+function flatten(arr, newArr = []) {
+  if (arr.length === 0) return newArr
+  if (Array.isArray(arr[0])) {
+    return flatten(arr[0].concat(arr.slice(1)), newArr)
+  } else {
+    newArr.push(arr[0])
+    return flatten(arr.slice(1), newArr)
+  }
+
+  return newArr
+}
